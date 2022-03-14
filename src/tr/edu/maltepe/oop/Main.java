f3f58672d7f72ebf30a6d458f20fdf564d22703d
Ali <ali_efe0@outlook.com>
2022-03-14 15:28:09 +0300

    Push

--- src/tr/edu/maltepe/oop/Main.java
+++ src/tr/edu/maltepe/oop/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         Professor Ensar = new Professor("Ensar","Gul");
+
         Ensar.setclass_name("SE 237");
 
         Student Ali = new Student("Ali","Efe",200704019);
+
         Ali.enroll_class("SE 237");
 
         Ensar.teach(Ali);
